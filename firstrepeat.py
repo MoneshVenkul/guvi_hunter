632ff7bcaff5c8ec76aaac1c6845188caa58268e
MoneshVenkul <48666307+MoneshVenkul@users.noreply.github.com>
2019-03-25 18:10:46 +0530

    Create firstrepeat.py

---
+++ firstrepeat.py
@@ -0,0 +1,15 @@
+#monesh
+
+n=int(input())
+a=list(map(int,input().split()))
+b=[]
+t=0
+for i in a:
+    if i not in b:
+        b.append(i)
+    else:
+        print(i)
+        t+=1
+        break
+if t==0:
+    print("unique")
