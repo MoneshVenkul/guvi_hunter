50a3c4827eb257ac4aa6da59473e2c6930c8c7cb
MoneshVenkul <48666307+MoneshVenkul@users.noreply.github.com>
2019-03-25 18:49:02 +0530

    Create evenodd.py

---
+++ evenodd.py
@@ -0,0 +1,18 @@
+#monesh
+
+n=int(input())
+a=list(map(int,input().split()))
+b=[]
+for i in range(n):
+    if i%2==0:
+        if a[i]%2!=0:
+            b.append(a[i])
+    else:
+        if a[i]%2==0:
+            b.append(a[i])
+x=len(b)
+for j in range(x):
+    if j!=x-1:
+        print(b[j],end=' ')
+    else:
+        print(b[j])
