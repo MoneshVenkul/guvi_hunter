6e3ce2dce66ea4545c717b99cdcabd767edcbd03
MoneshVenkul <48666307+MoneshVenkul@users.noreply.github.com>
2019-03-25 13:54:06 +0530

    Create repeated.py

---
+++ repeated.py
@@ -0,0 +1,18 @@
+#Monesh
+n=int(input())
+a=list(map(int,input().split()))
+b=[]
+c=[]
+for x in a:
+    if x not in b:
+        b.append(x)
+    else:
+        c.append(x)
+d=set(c)
+e=list(d)
+e.sort()
+for i in e:
+    if i!=e[-1]:
+        print(i,end=' ')
+    else:
+        print(i)
