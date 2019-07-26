6daa75cfe7ba24b33b275bf8c17a8349d878a852
Monesh_Venkul <mvvenkul@gmail.com>
2019-07-26 14:21:56 +0530

    Update s4_04.py

--- s4_04.py
+++ s4_04.py
@@ -1,12 +1,25 @@
 
     
 import sys,string
-n,k = input().split()
-n,k = int(n),int(k)
-L = [ int(x) for x in input().split()]
-for i in range(0,n-1) :
-    for j in range(i+1,n) :
-        if L[i]+L[j] == k :
-            print('YES')
-            sys.exit()
-print('NO')
+from itertools import permutations
+
+s = input()
+L = list(permutations(s))
+L2 = [ ''.join(x) for x in L]
+L3 = list(set(L2))
+#print(L3)
+L4 = [ int(x) for x in L3]
+if len(L4) == 1 :
+    print('impossible')
+    sys.exit()
+n = int(s)
+#print(L4)
+max1 = max(L4)
+if max1 <= n :
+    print('impossible')
+    sys.exit()
+for i in range(0,len(L4)) :
+    if L4[i] > n :
+        if L4[i] < max1 :
+            max1 = L4[i]
+print(max1)
