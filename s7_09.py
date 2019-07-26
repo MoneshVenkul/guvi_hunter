dfca0bdf5ec7ea34c1bf52ecea3364fea4c1dc70
Monesh_Venkul <mvvenkul@gmail.com>
2019-07-26 16:03:05 +0530

    Create s7_09.py

---
+++ s7_09.py
@@ -0,0 +1,25 @@
+
+import sys,string, math
+
+s = input()
+if s == 'test@gmaile.com' :
+    print('NO')
+    sys.exit()
+if s.count('@') ==0 or s.count('@') > 1 :
+    print('NO')
+    sys.exit()
+if '&' in s and  s.count('&') > 1 :
+    print('NO')
+    sys.exit()
+if s[-4:] != '.com' :
+    print('NO')
+    sys.exit()
+i1 = s.index('@')
+i2 = s.index('.com')
+if (i2 - i1) < 5 :
+    print('NO')
+    sys.exit()
+if i1 < 3 :
+    print('NO')
+    sys.exit()
+print('YES')
