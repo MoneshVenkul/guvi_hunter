ea358f0d528fc7d912ec6c555ce937d1c732de40
Monesh_Venkul <mvvenkul@gmail.com>
2019-07-26 14:08:44 +0530

    Create s3_01.py

---
+++ s3_01.py
@@ -0,0 +1,16 @@
+
+import sys, string, math, itertools
+n,m = list(map(int,input().split()))
+L = []
+
+for i in range(0,n) :
+    L.append([])
+for i in range(0, n):
+    L[i] = list(map(int, input().split()))
+for i in range(0,n) :
+    if L[i].count(0) > 0 :
+        L[i] = [0 for x in range(m)]
+for i in range(0,n) :
+    for j in range(0,m-1) :
+        print(L[i][j],end=' ')
+    print(L[i][m-1])
