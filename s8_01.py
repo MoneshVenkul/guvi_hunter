0f47cff0f7d463e95be4c85f5766558e33bd5437
Monesh_Venkul <mvvenkul@gmail.com>
2019-07-26 16:06:30 +0530

    Create s8_01.py

---
+++ s8_01.py
@@ -0,0 +1,13 @@
+
+import sys,string
+s = input()
+s2 = s.replace('.','')
+L = s2.split()
+n = len(L)
+L2 = []
+for i in range(0,n) :
+    if (i+1)%2 == 1 :
+        L2.append(L[i][::-1])
+    else :
+        L2.append(L[i])
+print(*L2)
